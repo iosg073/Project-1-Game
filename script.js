cb529f9eb9e3d7e9fdf27b04539fff5d5c493d65
alam <walam@up.com>
2021-05-07 00:54:14 -0500

    Front end styling and css updates

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 
-// let name1=prompt( 'what is your name ');
-// let worldEnter='abv';
-
 
 let btnSubmit=document.getElementById('btnSubmit1')
-// console.log(btn)
 let totalScore=0;
 let totalChances=8;
 let c=0;
 let randWord=['brush','house','bird','pen','ball','chicken','program','sun','play','letter'];
 let test=randWord[Math.floor(Math.random()*randWord.length)];
 document.querySelector('.keyBoard').style.display='none';
-// document.getElementById('lbl').style.display='none';
 
 
+//////////////////////////////Random Word button /////////////////////////////
 
 document.getElementById('btnRand').addEventListener('click',function()
 
@@ -22,23 +18,16 @@ document.getElementById('btnRand').addEventListener('click',function()
     domtxt=document.getElementById('txtword')
     worldEnter=randWord[Math.floor(Math.random()*randWord.length)]
     w=worldEnter;
-    // domtxt.value=''
     domtxt.style.display="none";
-
-
     worldLength=worldEnter.length;
     console.log(worldLength)
-
     let counter=0;
     document.querySelector('.keyBoard').style.display='flex';
-    // document.getElementById('lbl').style.display='flex';
     txtboxDisap=document.getElementById('domtry');
-    // txtboxDisap.style.display= 'none';
     btnBox=document.querySelector('.buttonDiv');
     btnBox.style.display= 'none';
     mainBox=document.querySelector('.mainarea');
     console.log(btnBox)
-
     let keyA=document.getElementById('buttonA');
     let keyB=document.getElementById('buttonB');
     let keyC=document.getElementById('buttonC');
@@ -80,6 +69,9 @@ for (let i=0;i<worldEnter.length;i++)
 divwraper.classList.add("flexContainer")
 mainBox.prepend(divwraper)
 
+
+///////////////////////// Getting Input from screen keyboad ///////////////////////////////
+
 const wordGetter= function(eventObj)
             {
                 eventObj.preventDefault();
@@ -88,13 +80,11 @@ const wordGetter= function(eventObj)
                 {
                   totalChances--;
                   
-                  eventObj.target.style.visibility='hidden';
-                 
-                 
-                //   txtboxDisap.className='';
+                  eventObj.target.style.visibility='hidden';                      
+               
                 if(totalChances==0)
                     {
-                    btnBox.style.display='flex'
+                    btnBox.style.display='flex';
                     totalChances=8;
                     alert(` You lose, it was ( ${w} )`);
                     location.reload();
@@ -102,16 +92,15 @@ const wordGetter= function(eventObj)
                     document.getElementById('lbl').innerHTML=`Chances Remaining: ${totalChances}`;
                 } else
                 {
-                    eventObj.target.style.color='red'
-                  eventObj.target.style.border='red'
+                    eventObj.target.style.color='red';
+                  eventObj.target.style.border='red';
                 }        
 
-                 // console.log(worldEnter.indexOf(key))
-                        for(let i=0;i<worldEnter.length;i++)
+                for(let i=0;i<worldEnter.length;i++)
                             {
                                 if(worldEnter.indexOf(eventObj.target.textContent.toLowerCase())>-1)
                                     {
-                                        tt=document.getElementsByClassName('inputHolder')
+                                        tt=document.getElementsByClassName('inputHolder');
                                         tt[worldEnter.indexOf(eventObj.target.textContent.toLowerCase())].innerHTML=eventObj.target.textContent.toLowerCase();
                                         worldEnter=worldEnter.replace(eventObj.target.textContent.toLowerCase(), "%");
                                         console.log(worldEnter);
@@ -119,104 +108,81 @@ const wordGetter= function(eventObj)
                                     }
                             }
                      
-                            // console.log(eventObj.target.textContent.toLowerCase())
                         if(c===worldEnter.length)
                             {
-                                // btnBox.style.display='flex'
-                                // document.querySelector('.keyBoard').style.display='none';
                                 
-                                // divwraper.style.display='none'
-                                // c=0;
-                                //totalChances=8;
                                 alert(` You win! it is ( ${w} )`);
                                 location.reload();                           
                                
                             }
                             console.log(domtxt);
-                            // console.log(worldEnter.indexOf(eventObj.target.textContent.toLowerCase()))
-            }
                             
+            }
+                                
+    keyA.addEventListener('click',wordGetter);
 
+    keyB.addEventListener('click',wordGetter)
 
-keyA.addEventListener('click',wordGetter);
+    keyC.addEventListener('click',wordGetter)
 
+    keyD.addEventListener('click',wordGetter)
 
-keyB.addEventListener('click',wordGetter)
+    keyE.addEventListener('click',wordGetter)
 
-keyC.addEventListener('click',wordGetter)
+    keyF.addEventListener('click',wordGetter)
 
+    keyG.addEventListener('click',wordGetter)
 
-keyD.addEventListener('click',wordGetter)
+    keyH.addEventListener('click',wordGetter)
 
+    keyI.addEventListener('click',wordGetter)
 
-keyE.addEventListener('click',wordGetter)
+    keyJ.addEventListener('click',wordGetter)
 
+    keyK.addEventListener('click',wordGetter)
 
-keyF.addEventListener('click',wordGetter)
+    keyL.addEventListener('click',wordGetter)
 
-keyG.addEventListener('click',wordGetter)
+    keyM.addEventListener('click',wordGetter)
 
+    keyN.addEventListener('click',wordGetter)
 
-keyH.addEventListener('click',wordGetter)
+    keyO.addEventListener('click',wordGetter)
 
+    keyP.addEventListener('click',wordGetter)
 
-keyI.addEventListener('click',wordGetter)
+    keyQ.addEventListener('click',wordGetter)
 
+    keyR.addEventListener('click',wordGetter)
 
+    keyS.addEventListener('click',wordGetter)
 
-keyJ.addEventListener('click',wordGetter)
+    keyT.addEventListener('click',wordGetter)
 
-keyK.addEventListener('click',wordGetter)
+    keyU.addEventListener('click',wordGetter)
 
+    keyV.addEventListener('click',wordGetter)
 
-keyL.addEventListener('click',wordGetter)
+    keyW.addEventListener('click',wordGetter)
 
+    keyX.addEventListener('click',wordGetter)
 
-keyM.addEventListener('click',wordGetter)
+    keyY.addEventListener('click',wordGetter)
 
-keyN.addEventListener('click',wordGetter)
-
-keyO.addEventListener('click',wordGetter)
-
-
-keyP.addEventListener('click',wordGetter)
-
-
-keyQ.addEventListener('click',wordGetter)
-
-
-keyR.addEventListener('click',wordGetter)
-
-keyS.addEventListener('click',wordGetter)
-
-
-keyT.addEventListener('click',wordGetter)
-
-
-keyU.addEventListener('click',wordGetter)
-
-
-keyV.addEventListener('click',wordGetter)
-
-keyW.addEventListener('click',wordGetter)
-
-keyX.addEventListener('click',wordGetter)
-
-keyY.addEventListener('click',wordGetter)
-
-
-keyZ.addEventListener('click',wordGetter)
-
+    keyZ.addEventListener('click',wordGetter)
 
 });
 
 
 
+//////////////////////// Reset Button ////////////////////////////////
 
 
 document.getElementById('btnreset').addEventListener('click', function(){
     location.reload();   
 })
+
+////////////////////////Enter key feature to text box/////////////////
 
 document.getElementById('txtword').addEventListener('keydown', function(event)
 {
